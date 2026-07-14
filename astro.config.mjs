eb4699d2a64a2dbf6e821c010c705c522d8cd3cb
Jacson Rodrigues Correia-Silva <jacsonrcsilva@gmail.com>
2026-07-14 03:36:00 -0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://www.jeiks.net',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
